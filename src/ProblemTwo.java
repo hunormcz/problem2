db243b45ba68978173af7bf3ad55af8acdeee9c0
Hunor <morocz_hunor@yahoo.com>
2015-07-24 15:08:09 +0300

    24/07
    test

--- src/ProblemTwo.java
+++ src/ProblemTwo.java
@@ -5,23 +5,23 @@ class ProblemTwo {
 
   public static void main (String args[]) throws IOException {
 	
+	  // read number of players
 	  String input; 
 	  BufferedReader stdin = new BufferedReader ( new InputStreamReader( System.in ) );
+	    
 	  System.out.println("How many players per team?");
 	  input = stdin.readLine();
 	  int pnumbers =  Integer.valueOf(input);
 	  
+	  // build team composition
 	  Units unit = new Units();
-	  String[] teamcomp = unit.unitComposition(pnumbers);
-	  
 	  printTeam team = new printTeam();
-	  
+	  String[] teamcomp = unit.unitComposition(pnumbers);
 	
+	  // add random names from files
 	  readNames readn = new readNames();
 	  String[] playernames =  readNames.readFile(pnumbers); 
-	  
-		
-	  
+	    
 	  team.print(teamcomp, playernames, pnumbers);
 	  
   }
